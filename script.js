8bc043548f48e3f52ff7cd45102df0717605e6bc
prasantahembram <Prasantahembram720@gmail.com>
2022-09-08 00:48:52 +0530

    Added new features
    * Added new template
    * Improved design

--- script.js
+++ script.js
@@ -70,3 +70,13 @@ function basicSearch() {
 
 // }
 
+      //show and hide dropdown list item on button click
+      function show_hide() {
+        var click = document.getElementById("list-items");
+        if(click.style.display ==="none") {
+           click.style.display ="block";
+        } else {
+           click.style.display ="none";
+        } 
+     }
+
